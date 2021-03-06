d2fa0baec06914b2665775b2527df92de9a3981e
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2021-03-06 18:21:44 +0530

    Update website_visitor.py

--- website_visitor.py
+++ website_visitor.py
@@ -7,10 +7,11 @@ urls = ['https://github.com/somj57/somj57/blob/master/README.md','https://github
 while count < 1000:
     for url in urls:
         webbrowser.open(url, new=0)
-        time.sleep(1)
-        # k.press_keys(['Command','W'])
+        time.sleep(1) #Change the according to your internet connection and device 
         count = count + 1
         print(count);
 
 else:
     pass
+
+# Above code may look foolish but trust me it works Just change the urls links to your dezired link
