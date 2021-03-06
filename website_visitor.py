cedad224ff7ae986094dea08ba5e7eab3a61116e
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2021-03-06 18:13:41 +0530

    Update website_visitor.py

--- website_visitor.py
+++ website_visitor.py
@@ -1,10 +1,8 @@
 import webbrowser
 import time
-# from pykeyboard import PyKeyboard
 
 count = 0
 urls = ['https://github.com/somj57/somj57/blob/master/README.md','https://github.com/somj57/somj57/blob/master/README.md','https://github.com/somj57/somj57/blob/master/README.md']
-# k = PyKeyboard()
 
 while count < 1000:
     for url in urls:
